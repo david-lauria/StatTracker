d48046a02438af5f361296b33897ca49eab8d4fa
Rex Borseth <borseth@iastate.edu>
2016-04-23 21:35:49 -0500

    moved some things arround, added example code to test

---
+++ main.js
@@ -0,0 +1,23 @@
+/**
+ * Created by Rex Borseth on 4/22/2016.
+ */
+
+src="//d3js.org/d3.v3.min.js"
+
+
+var makeUndergrad = function(){
+    displayGraph();
+}
+
+var makeGrad = function(){
+    
+}
+
+var displayGraph = function(){
+    document.getElementById("graph").innerHTML = generateGraph();
+}
+
+var generateGraph = function(){
+
+
+}
