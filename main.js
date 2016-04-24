83bbf539ff2d50fd30e8dcb63ee9151c23e2dc47
Rex Borseth <borseth@iastate.edu>
2016-04-23 22:06:19 -0500

    added d3.csv.parse and started setup for splitting data

--- main.js
+++ main.js
@@ -4,6 +4,26 @@
 
 src="//d3js.org/d3.v3.min.js"
 
+d3.csv.parse(name,function(d) {
+        return {
+            College: d.College,
+            Curriculum: d.Curriculum,
+            Major: d.Major,
+            Freshmen_M: d.Freshmen_M,
+            Freshmen_F: d.Freshmen_F,
+            Sophomores_M: d.Sophomores_M,
+            Sophomores_F: d.Sophomores_F,
+            Juniors_M: d.Juniors_M,
+            Juniors_F: d.Juniors_F,
+            Seniors_M: d.Seniors_M,
+            Seniors_F: d.Seniors_F,
+            Specials_M: d.Specials_M,
+            Specials_F: d.Specials_F,
+            Grand_Total: d.Grand_Total
+        };
+    }
+
+)
 
 var makeUndergrad = function(){
     displayGraph();
