c4dd7b93aad04d00ca60ce82658fb49d78344a4d
dtlauria <dtlauria@iastate.edu>
2016-04-24 12:23:53 -0500

    need to find way to only get values from selector values

--- main.js
+++ main.js
@@ -108,3 +108,54 @@ var generateGraph = function(){
 
 
 }
+
+
+var generateComparisonGraph = function(){
+    var width = 500,
+        height = 500,
+        radius = Math.min(width, height) / 2;
+
+    var color = d3.scale.ordinal()
+        .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+
+    var arc = d3.svg.arc()
+        .outerRadius(radius - 10)
+        .innerRadius(radius - 150);
+
+    var pie = d3.layout.pie()
+        .sort(null)
+        .value(function(d) { return d.population; });
+
+    var svg = d3.select("#workspace").append("svg")
+        .attr("width", width)
+        .attr("height", height)
+        .append("g")
+        .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+
+    d3.csv(name , type, function(error, data) {// need to add the formated info in the first argument
+        if (error) throw error;
+
+
+        // somewhere in this area below i need to use only the values in selector and selector2 and just return that data.
+        var g = svg.selectAll(".arc")
+            .data(pie(data))
+            .enter().append("g")
+            .attr("class", "arc");
+
+        g.append("path")
+            .attr("d", arc)
+            .style("fill", function(d) { return color(d.data.major); });
+
+        g.append("text")
+            .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
+            .attr("dy", ".35em")
+            .text(function(d) { return d.data.major; });
+    });
+
+    function type(d) {
+        d.population = +d.population;
+        return d;
+    }
+
+
+}
