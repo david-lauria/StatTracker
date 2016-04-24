b1d48d84be065a7d982465b49f3fc2b0b3aff7d1
Rex Borseth <borseth@iastate.edu>
2016-04-24 15:43:22 -0500

    turned main pie charts into bar charts, added Grand_Total.csv , formatted bars to look nice

--- main.js
+++ main.js
@@ -32,6 +32,37 @@ var makeUndergrad = function(){
 }*/
 
 var generateGraph = function(){
+
+    //begin bar stuff
+
+    var margin = {top: 20, right: 20, bottom: 400, left: 100},
+        width = 1050 - margin.left - margin.right,
+        height = 700 - margin.top - margin.bottom;
+
+    var x = d3.scale.ordinal()
+        .rangeRoundBands([0, width], .1);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom");
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .orient("left")
+        .ticks(10, "");
+
+    var svg = d3.select("#workspace").append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+    //end bar stuff
+
+/*
     var width = 500,
         height = 500,
         radius = Math.min(width, height) / 2;
@@ -52,8 +83,9 @@ var generateGraph = function(){
         .attr("height", height)
         .append("g")
         .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+*/
 
-    d3.csv(name , type, function(error, data) {// need to add the formated info in the first argument
+    d3.csv(name , type, function(error, data) {
         if (error) throw error;
 
     var select = d3.select("#selector")
@@ -83,8 +115,43 @@ var generateGraph = function(){
         .attr("major", function (d) { return d.major; })
         .text(function (d) { return d.major; });
 
+        //star bar
 
+        x.domain(data.map(function(d) { return d.major; }));
+        y.domain([0, d3.max(data, function(d) { return d.population; })]);
 
+        svg.append("g")
+            .attr("class", "x axis")
+            .attr("transform", "translate(0," + height + ")")
+            .call(xAxis)
+            .selectAll("text")
+                .style("text-anchor","end")
+                .attr("dx","-.5em")
+                .attr("dy",".15em")
+                .attr("transform", "rotate(-70)");
+
+        svg.append("g")
+            .attr("class", "y axis")
+            .call(yAxis)
+            .append("text")
+            .attr("transform", "rotate(-90)")
+            .attr("y", 6)
+            .attr("dy", ".71em")
+            .style("text-anchor", "end")
+            .text("Number of Students");
+
+        svg.selectAll(".bar")
+            .data(data)
+            .enter().append("rect")
+            .attr("class", "bar")
+            .attr("x", function(d) { return x(d.major); })
+            .attr("width", x.rangeBand())
+            .attr("y", function(d) { return y(d.population); })
+            .attr("height", function(d) { return height - y(d.population); });
+
+        //end bar
+
+/*
 
         var g = svg.selectAll(".arc")
             .data(pie(data))
@@ -99,9 +166,11 @@ var generateGraph = function(){
             .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
             .attr("dy", ".35em")
             .text(function(d) { return d.data.major; });
+*/
     });
 
     function type(d) {
+        d.major = d.major;
         d.population = +d.population;
         return d;
     }
